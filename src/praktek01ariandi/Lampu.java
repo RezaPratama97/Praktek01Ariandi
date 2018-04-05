23fccf92b975a3693482d820861395bdf5e88b61
Ariandi <>
2018-04-06 00:20:48 +0700

    Membuat atribut dan behavior

--- src/praktek01ariandi/Lampu.java
+++ src/praktek01ariandi/Lampu.java
@@ -10,5 +10,15 @@ package praktek01ariandi;
  * @author ACER-PC
  */
 public class Lampu {
+    String merk;
+    String harga;
+    String warna;
+    String jenis;
     
+    void cetakInfo(){
+        System.out.println("merk :"+merk);
+        System.out.println("harga :"+harga);
+        System.out.println("warna :"+warna);
+        System.out.println("jenis :"+jenis);
+    }
 }
